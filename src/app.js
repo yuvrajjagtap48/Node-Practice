f7dcc005e9ba17be14923cf1f6da7adcc2b075c7
yuvrajjagtap48 <yuvrajjagtap2004@gmail.com>
2025-12-01 19:11:06 +0530

    Schema validation

--- src/app.js
+++ src/app.js
@@ -278,10 +278,17 @@ app.patch("/user", async (req, res) => {
     const userId = req.body.userId;
     const data = req.body;
     try {
-        const user = await User.findByIdAndUpdate({ _id: userId }, data);
+        const user = await User.findByIdAndUpdate(
+            { _id: userId },
+            data,
+            {
+                returnDocument: "after",
+                runValidators: true,
+            }
+        );
         res.send("User data updated successfully");
     } catch (err) {
-        res.status(500).send("Error updating user data");
+        res.status(400).send("Error updating user data");
     }
 });
 
